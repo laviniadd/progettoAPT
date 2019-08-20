be21f3da148570b0655f75b950048b7a05049664
laviniadd <lavinia@dd.it>
2019-08-20 11:35:57 +0200

    initial import

---
+++ progetto/src/main/java/com/myproject/app/progetto/App.java
@@ -0,0 +1,13 @@
+package com.myproject.app.progetto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
