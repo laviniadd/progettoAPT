d8b2917a019fa0d8c8d126e6b6b95367a92647d7
laviniadd <lavinia@dd.it>
2019-08-26 10:09:54 +0200

    docker files, plugin

--- progetto/src/main/java/com/myproject/app/progetto/App.java
+++ progetto/src/main/java/com/myproject/app/progetto/App.java
@@ -1,13 +1,24 @@
 package com.myproject.app.progetto;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+/* import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import model.Listadellaspesa;
+*/
+
+public class App {
+	public static void main(String[] args) {
+		 System.out.println( "Hello World!" );
+		/* EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("production");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		Listadellaspesa listaspesa = new Listadellaspesa();
+		listaspesa.setName("listaprova");
+		entityManager.getTransaction().begin();
+		entityManager.persist(listaspesa);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();*/
+	}
 }
