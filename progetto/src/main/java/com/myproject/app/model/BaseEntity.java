bf0bbe9c239f80a532921fcad9efbfce276aadf5
laviniadd <lavinia@dd.it>
2019-08-28 16:04:10 +0200

    autogenerated hascode and equals. manca uuid

--- progetto/src/main/java/com/myproject/app/model/BaseEntity.java
+++ progetto/src/main/java/com/myproject/app/model/BaseEntity.java
@@ -6,9 +6,44 @@ import javax.persistence.*;
 public abstract class BaseEntity {
 
 	private Long id;
-	private String uuid;
+//	private String uuid;
 
 	protected BaseEntity() {
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseEntity other = (BaseEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	/*
@@ -20,15 +55,7 @@ public abstract class BaseEntity {
 	}
 	*/
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	
 	/*	@Column(unique = true)
 	public String getUuid() {
 		return uuid;
@@ -38,6 +65,8 @@ public abstract class BaseEntity {
 		this.uuid = uuid;
 	}
 */
+
+	/*
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -68,6 +97,6 @@ public abstract class BaseEntity {
 			return false;
 		return true;
 	}
-	
+	*/
 	
 }
