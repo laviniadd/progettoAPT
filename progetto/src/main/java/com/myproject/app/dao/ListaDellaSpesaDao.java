776d3bc01b1719fa8991356a22a4b8eaacad5842
laviniadd <lavinia@dd.it>
2019-09-05 17:05:10 +0200

    comment removed

--- progetto/src/main/java/com/myproject/app/dao/ListaDellaSpesaDao.java
+++ progetto/src/main/java/com/myproject/app/dao/ListaDellaSpesaDao.java
@@ -1,18 +1,11 @@
 package com.myproject.app.dao;
 
-//import java.util.List;
-
-//import javax.persistence.*;
-
 import com.myproject.app.model.ListaSpesa;
 
 public class ListaDellaSpesaDao extends BaseDao<ListaSpesa> {
 
-	//private EntityManager entityManager;
-
 	public ListaDellaSpesaDao() {
 		super(ListaSpesa.class);
-//		this.entityManager = PersistenceManager.getEntityManager();
 	}
 
 
