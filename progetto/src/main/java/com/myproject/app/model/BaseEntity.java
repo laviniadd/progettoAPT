feed393fc5d7f9daa335064e868fcc879d54bdac
laviniadd <lavinia@dd.it>
2019-09-09 15:54:07 +0200

    fixed base entity code smell

--- progetto/src/main/java/com/myproject/app/model/BaseEntity.java
+++ progetto/src/main/java/com/myproject/app/model/BaseEntity.java
@@ -40,8 +40,9 @@ public abstract class BaseEntity {
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!id.equals(other.id)) {
 			return false;
+		}
 		return true;
 	}
 
