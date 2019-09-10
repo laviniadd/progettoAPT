e6f27e64eb4f448b26f309358536d88c39eafe27
laviniadd <lavinia@dd.it>
2019-09-10 17:10:12 +0200

    Subclasses that add fields should override "equals" fixed

--- progetto/src/main/java/com/myproject/app/model/Prodotto.java
+++ progetto/src/main/java/com/myproject/app/model/Prodotto.java
@@ -21,4 +21,21 @@ public class Prodotto extends BaseEntity {
 		this.name = name;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prodotto other = (Prodotto) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
