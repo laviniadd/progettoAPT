49ab7ac94343cbb5a9aabf2969f7b482523dff93
laviniadd <lavinia@dd.it>
2019-09-16 12:19:02 +0200

    added listaspesaview and controller test

---
+++ progetto/src/main/java/com/myproject/app/view/ListaSpesaView.java
@@ -0,0 +1,14 @@
+package com.myproject.app.view;
+
+import java.util.List;
+
+import com.myproject.app.model.ListaSpesa;
+
+public class ListaSpesaView {
+
+	public void showAllListeSpesa(List<ListaSpesa> listeSpesa) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+}
--- progetto/src/test/java/com/myproject/app/controller/ListaSpesaControllerTest.java
+++ progetto/src/test/java/com/myproject/app/controller/ListaSpesaControllerTest.java
@@ -2,42 +2,43 @@ package com.myproject.app.controller;
 
 import java.util.List;
 
-import javax.xml.ws.Response;
-
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import static org.mockito.Mockito.when;
+
 
 import com.myproject.app.dao.ListaDellaSpesaDao;
 import com.myproject.app.model.ListaSpesa;
+import com.myproject.app.view.ListaSpesaView;
 
 public class ListaSpesaControllerTest {
 	@InjectMocks
 	private ListaSpesaController listaSpesaController;
 	@Mock
 	private ListaDellaSpesaDao listaSpesaDao;
+	@Mock
+	private ListaSpesaView listaSpesaView;
 
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 	}
 
-	/*
-	 * @Test public void testAllListaSpesa() {
-	 * 
-	 * ListaSpesa ls = new ListaSpesa(); List<ListaSpesa> listeSpesa;
-	 * listeSpesa.add(ls);
-	 * 
-	 * when(listaSpesaDao.findAll()).thenReturn(listeSpesa);
-	 * 
-	 * 
-	 * 
-	 * Response result = listaSpesaController.allListeSpesa();
-	 * 
-	 * }
-	 */
+	@Test
+	public void testAllListaSpesa() {
+
+		List<ListaSpesa> listeSpesa = asList(new ListaSpesa());
+
+		when(listaSpesaDao.findAll()).thenReturn(listeSpesa);
+
+		listaSpesaController.allListeSpesa();
+		
+		verify(listaSpesaView).showAllListeSpesa(listeSpesa);
+
+	}
 
 }
