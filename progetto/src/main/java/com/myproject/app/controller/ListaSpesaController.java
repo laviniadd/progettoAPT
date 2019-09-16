abad41bb94c729c10be8014670070e43ff5bf07b
laviniadd <lavinia@dd.it>
2019-09-16 11:52:39 +0200

    up

--- progetto/src/main/java/com/myproject/app/controller/ListaSpesaController.java
+++ progetto/src/main/java/com/myproject/app/controller/ListaSpesaController.java
@@ -1,5 +1,12 @@
 package com.myproject.app.controller;
+import java.util.List;
 
+import com.myproject.app.model.ListaSpesa;
 public class ListaSpesaController {
+
+	public List<ListaSpesa> allListeSpesa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- progetto/src/test/java/com/myproject/app/controller/ListaSpesaControllerTest.java
+++ progetto/src/test/java/com/myproject/app/controller/ListaSpesaControllerTest.java
@@ -2,11 +2,14 @@ package com.myproject.app.controller;
 
 import java.util.List;
 
+import javax.xml.ws.Response;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
 
 import com.myproject.app.dao.ListaDellaSpesaDao;
 import com.myproject.app.model.ListaSpesa;
@@ -21,12 +24,20 @@ public class ListaSpesaControllerTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	/*
-	 * @Test public void testAllListaSpesa() { List<ListaSpesa> students;
+	 * @Test public void testAllListaSpesa() {
 	 * 
-	 * when(studentRepository.findAll()) .thenReturn(students);
-	 * schoolController.allStudents(); verify(studentView)
-	 * .showAllStudents(students); }
+	 * ListaSpesa ls = new ListaSpesa(); List<ListaSpesa> listeSpesa;
+	 * listeSpesa.add(ls);
+	 * 
+	 * when(listaSpesaDao.findAll()).thenReturn(listeSpesa);
+	 * 
+	 * 
+	 * 
+	 * Response result = listaSpesaController.allListeSpesa();
+	 * 
+	 * }
 	 */
+
 }
