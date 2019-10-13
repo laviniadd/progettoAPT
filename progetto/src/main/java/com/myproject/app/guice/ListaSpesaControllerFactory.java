152a98f7dcdb0959555ccbc85325238996eb5d6e
laviniadd <lavinia@dd.it>
2019-10-13 15:36:55 +0200

    project clean

--- progetto/src/main/java/com/myproject/app/guice/ListaSpesaControllerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.myproject.app.guice;
-
-import com.myproject.app.controller.ListaSpesaController;
-import com.myproject.app.view.AppSwingView;
-
-public interface ListaSpesaControllerFactory {
-	ListaSpesaController create(AppSwingView view);
-}
--- progetto/src/main/java/com/myproject/app/guice/ProdottoControllerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.myproject.app.guice;
-
-import com.myproject.app.controller.ProdottoController;
-import com.myproject.app.view.AppSwingView;
-
-public interface ProdottoControllerFactory {
-	ProdottoController create(AppSwingView view);
-}
